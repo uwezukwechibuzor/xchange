ecbbe35bdcf301c2c675389accb00909828011b1
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2023-12-14 22:50:53 +0100

    Add Sell Orders

--- x/dex/keeper/msg_server_sell_order.go
+++ x/dex/keeper/msg_server_sell_order.go
@@ -2,34 +2,47 @@ package keeper
 
 import (
 	"context"
+	"errors"
+
+	"xchange/x/dex/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
-	"xchange/x/dex/types"
 )
 
 func (k msgServer) SendSellOrder(goCtx context.Context, msg *types.MsgSendSellOrder) (*types.MsgSendSellOrderResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: logic before transmitting the packet
+	// If an order book doesn't exist, throw an error
+	pairIndex := types.OrderBookIndex(msg.Port, msg.ChannelID, msg.AmountDenom, msg.PriceDenom)
+	_, found := k.GetSellOrderBook(ctx, pairIndex)
+	if !found {
+		return &types.MsgSendSellOrderResponse{}, errors.New("the pair doesn't exist")
+	}
 
-	// Construct the packet
+	// Get sender's address
+	sender, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return &types.MsgSendSellOrderResponse{}, err
+	}
+
+	// Use SafeBurn to ensure no new native tokens are minted
+	if err := k.SafeBurn(ctx, msg.Port, msg.ChannelID, sender, msg.AmountDenom, msg.Amount); err != nil {
+		return &types.MsgSendSellOrderResponse{}, err
+	}
+
+	// Save the voucher received on the other chain, to have the ability to resolve it into the original denom
+	k.SaveVoucherDenom(ctx, msg.Port, msg.ChannelID, msg.AmountDenom)
+
 	var packet types.SellOrderPacketData
-
+	packet.Seller = msg.Creator
 	packet.AmountDenom = msg.AmountDenom
 	packet.Amount = msg.Amount
 	packet.PriceDenom = msg.PriceDenom
 	packet.Price = msg.Price
 
 	// Transmit the packet
-	_, err := k.TransmitSellOrderPacket(
-		ctx,
-		packet,
-		msg.Port,
-		msg.ChannelID,
-		clienttypes.ZeroHeight(),
-		msg.TimeoutTimestamp,
-	)
+	_, err = k.TransmitSellOrderPacket(ctx, packet, msg.Port, msg.ChannelID, clienttypes.ZeroHeight(), msg.TimeoutTimestamp)
 	if err != nil {
 		return nil, err
 	}
